bec243dae439cd54fb4bddc302e1ec3a95f58685
Corbin <cpwest11@gmail.com>
2013-01-22 15:20:49 -0600

    Bullets! The go up now!

--- THE MASTER/Assets/1 BlueSteel/Wepon Scripts/Bullet.js
+++ THE MASTER/Assets/1 BlueSteel/Wepon Scripts/Bullet.js
@@ -1,5 +1,9 @@
 var lifeTime = 3;
 function Start() {
-	yield WaitForSeconds(lifeTime);
-	Destroy(this.gameObject);
+    yield WaitForSeconds(lifeTime);
+    Destroy(this.gameObject);
+}
+function OnCollisionEnter() {
+
+Destroy (gameObject);
 }
